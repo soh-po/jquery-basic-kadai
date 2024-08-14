078fa33978bd5903d14fa314974472f0cb31464f
soh-po <soheiozawa0174@gmail.com>
2024-08-14 22:06:08 +0900

    Add files via upload

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,13 +1,17 @@
 $(function() {
+    // 文字色を変える
     $('#change-color').on('click', function() {
         $('#target').css('color', 'green');
     });
+    // 文字を変える
     $('#change-text').on('click', function() {
         $('#target').text('Hello!');
     });
+    // 文字をフェードアウトさせる
     $('#fade-out').on('click', function() {
         $('#target').fadeOut();
     });
+    // 文字をフェードインさせる
     $('#fade-in').on('click', function() {
         $('#target').fadeIn();
     });
